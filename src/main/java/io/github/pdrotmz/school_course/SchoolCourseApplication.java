9aa015df59de0aa59a2fa4fa16df0dc66a10d848
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-09 15:19:44 -0300

    initial commit

---
+++ src/main/java/io/github/pdrotmz/school_course/SchoolCourseApplication.java
@@ -0,0 +1,16 @@
+package io.github.pdrotmz.school_course;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCourseApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		SpringApplication.run(SchoolCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pdrotmz/school_course/SchoolCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pdrotmz.school_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
